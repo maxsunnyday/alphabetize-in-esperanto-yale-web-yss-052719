c141406e1fd5ba002a625bd6ab50852a1b4fd88f
Max Sun <max.sun@yale.edu>
2019-05-21 05:28:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   alphabet_array = alphabet.split("")
   arr.sort_by do |word|
-    word.split("").map do |character|
+    word.split("").collect do |letter|
       alphabet_array.index(character)
     end
   end
