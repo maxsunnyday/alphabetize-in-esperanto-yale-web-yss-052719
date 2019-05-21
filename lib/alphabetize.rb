ac092b7af55eece34dd8a072f3c5ec74da6491a7
Max Sun <max.sun@yale.edu>
2019-05-21 05:28:14 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ def alphabetize(arr)
   alphabet_array = alphabet.split("")
   arr.sort_by do |word|
     word.split("").collect do |letter|
-      alphabet_array.index(character)
+      alphabet_array.index(letter)
     end
   end
 end
