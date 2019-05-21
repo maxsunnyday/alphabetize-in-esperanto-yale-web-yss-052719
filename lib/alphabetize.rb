4e5ce3ea543c963220c70ff4bd72cc6bbacb9439
Max Sun <max.sun@yale.edu>
2019-05-21 05:26:04 +0000

    Esperanto

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require "pry"
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet_array = alphabet.split("")
+  arr.sort_by do |word|
+    word.split("").map do |character|
+      alphabet_array.index(character)
+    end
+  end
 end
